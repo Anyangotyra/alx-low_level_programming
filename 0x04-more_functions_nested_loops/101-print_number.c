a25790a23bf3c1a6bd3feaed4eec50c2f30eccdd
Anyangotyra <tyrasophie@students.uonbi.ac.ke>
2023-08-18 16:00:13 -0700

    print number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,8 @@
-#include "main.h" 
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_number - Prints an integer.
- *
- * @n: The integer to print.
- *
- * Return: void
- */
-
+* print_number - that prints an integer
+*@n: print int
+* Return: Always 0.
+*/
 void print_number(int n)
 {
-	unsigned int k = n;
-
-	if (n < 0)
-	{
-		n *= -1;
-		k = n;
-		_putchar('-');
-	}
-
-	k /= 10;
-
-	if (k != 0)
-		print_number(k);
-
-	_putchar((unsigned int) n % 10 + '0');
-}
