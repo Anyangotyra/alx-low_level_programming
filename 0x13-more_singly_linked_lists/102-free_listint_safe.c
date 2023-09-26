b9224201f143d090b33de48fac3d52dc474633f9
Anyangotyra <tyrasophie@students.uonbi.ac.ke>
2023-09-26 11:49:21 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,19 +8,19 @@
  */
 void free_listp2(listp_t **head)
 {
-	listp_t *temp;
-	listp_t *curr;
+listp_t *temp;
+listp_t *curr;
 
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
+if (head != NULL)
+{
+curr = *head;
+while ((temp = curr) != NULL)
+{
+curr = curr->next;
+free(temp);
+}
+*head = NULL;
+}
 }
 
 /**
